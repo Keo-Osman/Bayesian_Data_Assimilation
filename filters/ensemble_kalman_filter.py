58c54450c03166ae4895185ef13b795de519934c
Keo <keo.osman77@gmail.com>
2026-02-24 21:26:41 +0000

    Updated Kalman gain calcluation in EnFK to be more consitent with KF

--- filters/ensemble_kalman_filter.py
+++ filters/ensemble_kalman_filter.py
@@ -8,10 +8,10 @@ def propagate(distribution: ParticleDistribution, transition, Q: np.ndarray, rng
 def update(distribution: ParticleDistribution, observation: np.ndarray, 
            H: np.ndarray, R: np.ndarray, rng: np.random.Generator):
     
-    cov = distribution.covariance
-    S = H @ cov @ H.T + R
-    K = cov @ H.T @ np.linalg.solve(S, np.eye(len(S)))
-    # K = cov @ H.T @ np.linalg.pinv(S)
+    P = distribution.covariance
+    S = H @ P @ H.T + R # Innovation vector covariance
+    K = np.linalg.solve(S.T, (P @ H.T).T).T # Kalman Gain
+    
 
     for i, particle in enumerate(distribution.particles):
         z = observation + rng.multivariate_normal(np.zeros(len(observation)), R)
